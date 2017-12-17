b801bfc46b79ac2ec8867008a55bc0bd41ede15b
Connor Novak <cnovak@olin.edu>
2017-12-17 13:18:14 -0500

    Added delay before sensor reading to stabilize distance readings

--- arduino/arduino_drawer/arduino_drawer.ino
+++ arduino/arduino_drawer/arduino_drawer.ino
@@ -1,56 +1,55 @@
+// include libraries
 #include <Servo.h>
-// this constant won't change. It's the pin number of the sensor's output:
-const int pingPin = 6;
-const int echoPin = 5;
+
 const int switchPin = 4;
+const int echoPin = 5;
+const int pingPin = 6;
+#define CANDY_SERVO_PIN 9
+#define TOKEN_SERVO_PIN 10
+
+#define BROADCAST_INTERVAL 500        // Also used for doing handshake
+#define ROTARY_TELEGRAPH 20
+#define ROTARY_CLOSED 120
+#define ROTARY_TRELLIS 180  
+#define HEARTBEAT_TIMEOUT 10000
+
 bool lastSwitchValue = 1;
 bool handshakeCompleted = false;     // Set to true when registration with computer completed
 bool usingHandshake = true;
 unsigned long nextBroadcastTime = 0;  // Used for doing handshake
-#define BROADCAST_INTERVAL 500        // Also used for doing handshake
 // Go back into handshake mode after 3 seconds of no heartbeat from computer
 unsigned long expectedHeartbeatByTime = 0;
-#define HEARTBEAT_TIMEOUT 10000
-Servo rotaryServo;
-#define ROTARY_TELEGRAPH 20
-#define ROTARY_CLOSED 120
-#define ROTARY_TRELLIS 180
+
+Servo candyServo;
+Servo tokenServo;
 
 void setup() {
   // initialize serial communication:
   Serial.begin(9600);
-  rotaryServo.attach(9);
+  
+  candyServo.attach(CANDY_SERVO_PIN);
+  tokenServo.attach(TOKEN_SERVO_PIN);
 
   pinMode(pingPin, OUTPUT);
   pinMode(echoPin, INPUT);
 }
 
-void sendPing(int pin) {
-  // The PING))) is triggered by a HIGH pulse of 2 or more microseconds.
-  // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
-  digitalWrite(pin, LOW);
-  delayMicroseconds(2);
-  digitalWrite(pin, HIGH);
-  delayMicroseconds(5);
-  digitalWrite(pin, LOW);
-  
-}
 
 void loop() {
   if (handshakeCompleted || !usingHandshake) {
 
     checkSerialForMessages();
     
-    // SENSOR
-    sendPing(pingPin);
-    short duration = pulseIn(echoPin, HIGH);
-    float cm = microsecondsToCentimeters(duration) - 30.0;
-    
     // SWITCH
     int testSwitch = digitalRead(switchPin);
     
     // Send readings to computer when drawer is closed
     if (testSwitch != lastSwitchValue) {
+      // SENSOR
+      delay(500); // Let readings stabilize
+      short duration = (takeReading(pingPin, echoPin) + takeReading(pingPin, echoPin) + takeReading(pingPin, echoPin))/3;
+      float cm = microsecondsToCentimeters(duration);
+        
       if (testSwitch == 1) { // Door open
         Serial.print("{\"event_id\": \"3\", \"data\": \"");
         Serial.print(cm);
@@ -86,6 +85,20 @@ void loop() {
     }
   }
 }
+
+
+void sendPing(int pin) {
+  // The PING))) is triggered by a HIGH pulse of 2 or more microseconds.
+  // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
+  digitalWrite(pin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(pin, HIGH);
+  delayMicroseconds(5);
+  digitalWrite(pin, LOW);
+  
+}
+
+
 void checkSerialForMessages() {
 
   if (Serial.available() > 0) {
@@ -99,12 +112,10 @@ void checkSerialForMessages() {
     String payload = msg.substring(1); // The message payload is everything beyond the output identifier
 
     if (outputId == 'R') { // Turn start button LED ring on/off
-      if (payload[0] == 'T') {
-        rotaryServo.write(ROTARY_TELEGRAPH);
-      } else if (payload[0] == 'C') {
-        rotaryServo.write(ROTARY_CLOSED);
-      } else if (payload[0] == 'A') {
-        rotaryServo.write(ROTARY_TRELLIS);
+      if (payload[0] == 'C') {
+        dispenseItem('c');
+      } else if (payload[0] == 'T') {
+        dispenseItem('t');
       }
     }
   }
@@ -115,3 +126,26 @@ float microsecondsToCentimeters(long microseconds) {
   // take half of the distance travelled.
   return ((float)microseconds) / 14.5;
 }
+
+void dispenseItem(char item) {
+  // Dependent on which item to dispense, runs the dispensing process on
+  // the appropriate servo
+  if (item == 'c') {
+    Serial.println("dispensing candy");
+    candyServo.write(110);
+    delay(2200);
+    candyServo.write(90);
+  } else if (item == 't') {
+    Serial.println("dispensing token");
+    tokenServo.write(90);
+    delay(500);
+    tokenServo.write(70);
+  }
+}
+
+short takeReading(int trig, int echo) {
+  sendPing(trig);
+  short duration = pulseIn(echo, HIGH);
+  return duration;
+}
+
